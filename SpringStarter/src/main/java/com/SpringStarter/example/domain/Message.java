1efd281276355498ce751719905067f4e52c38c8
aeweqsd <67893258+aeweqsd@users.noreply.github.com>
2020-08-24 18:51:44 +0900

    8-24-2
    8-24  쪽지 db 추가

---
+++ SpringStarter/src/main/java/com/SpringStarter/example/domain/Message.java
@@ -0,0 +1,46 @@
+package com.SpringStarter.example.domain;
+
+public class Message {
+	private int id;
+	private int sender;
+	private int receiver;
+	private String content;
+	private String name;
+	private boolean is_read;
+	public boolean isIs_read() {
+		return is_read;
+	}
+	public void setIs_read(boolean is_read) {
+		this.is_read = is_read;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getSender() {
+		return sender;
+	}
+	public void setSender(int sender) {
+		this.sender = sender;
+	}
+	public int getReceiver() {
+		return receiver;
+	}
+	public void setReceiver(int receiver) {
+		this.receiver = receiver;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
